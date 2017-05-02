b9d3064ea898fd8c201fd040dc5dc0d23d2d7549
Willie Maddox <willie.maddox@gmail.com>
2017-05-02 07:00:55 -0500

    add scripts to handle io and other handy generic functions.

---
+++ IO.py
@@ -0,0 +1,260 @@
+import os
+import shutil
+from operator import itemgetter
+from PIL import Image
+import networkx as nx
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle as pickle
+import utils
+
+data_source_dir = "/media/Borg_LS/DATA"
+# data_source_dir = "/media/RED6/DATA"
+imagenet_source_dir = os.path.join(data_source_dir, "imagenet")
+wordnet_source_dir = os.path.join(data_source_dir, "nltk_data", "corpora", "wordnet")
+pixabay_source_dir = os.path.join(data_source_dir, "pixabay")
+pixabay_image_dir = os.path.join(pixabay_source_dir, "JPEGImages")
+pixabay_orphans_dir = os.path.join(pixabay_source_dir, "orphans")
+
+
+def load_txt(filename, col_delim=None):
+    with open(filename) as ifs:
+        data = ifs.read().strip().split('\n')
+    if col_delim is not None:
+        words = []
+        for line in data:
+            words.append(line.split(col_delim))
+        data = words
+    return data
+
+
+def load_pkl(filename):
+    """ Wrapper for pickle.load() """
+    if os.path.exists(filename):
+        with open(filename, 'rb') as ifs:
+            pkl_obj = pickle.load(ifs)
+    else:
+        pkl_obj = {}
+    return pkl_obj
+
+
+def dump_pkl(filename, pkl_obj):
+    """ Wrapper for pickle.dump() """
+    if len(pkl_obj) > 0:
+        with open(filename, 'wb') as ofs:
+            pickle.dump(pkl_obj, ofs, pickle.HIGHEST_PROTOCOL)
+    return
+
+
+def read_wordnet_heirarchy_file():
+    filename = imagenet_source_dir + "wordnet.is_a.txt"
+    graph = nx.read_edgelist(filename, create_using=nx.DiGraph())
+    assert nx.is_directed_acyclic_graph(graph)
+    return graph
+
+
+def read_synset_words_file():
+    filename = imagenet_source_dir + "synset_words.txt"
+    with open(filename) as ifs:
+        lines = ifs.read().strip().split('\n')
+    synset_words = [line.split('\t') for line in lines]
+    synsets, word_strings = zip(*synset_words)
+    word_lists = [[w.strip() for w in ws.split(',')] for ws in word_strings]
+    synset_words_dict = {synset: word_list for synset, word_list in zip(synsets, word_lists)}
+    return synset_words_dict
+
+
+def get_whitelist_words():
+    synset_words_dict = read_synset_words_file()
+    return set([s for ss in synset_words_dict.itervalues() for s in ss])
+
+
+def read_wordnet_index_file(part_of_speech, output=None):
+    if output is None:
+        output = []
+    filename = os.path.join(wordnet_source_dir, "index." + part_of_speech)
+    with open(filename) as ifs:
+        for line in ifs.readlines():
+            if line.startswith('  '):
+                continue
+            data = line.strip().split()[0].replace('_', ' ')
+            if isinstance(output, list):
+                output.append(data)
+            elif isinstance(output, set):
+                output.add(data)
+    return output
+
+
+def read_wordnet_exc_file(part_of_speech, output=None, verbose=False):
+    if verbose:
+        print part_of_speech
+    filename = os.path.join(wordnet_source_dir, part_of_speech + ".exc")
+    with open(filename) as ifs:
+        lines = ifs.read().strip().split('\n')
+    word_list2 = [l.split() for l in lines]
+    if output is None:
+        output = {}
+    if isinstance(output, dict):
+        output = {w[0]: w[1:] for w in word_list2}
+    elif isinstance(output, set):
+        # output2 = set()
+        for word_list in word_list2:
+            if verbose and len(word_list) > 2:
+                print word_list
+            for w in word_list:
+                output.add(w.replace('_', ' '))
+            # for i, w in enumerate(word_list):
+            #     if i == 0:
+            #         output.add(w.replace('_', ' '))
+            #     else:
+            #         output2.add(w.replace('_', ' '))
+            # output.add(word_list[1:])
+
+    return output
+
+
+def read_pixabay_metadata_file():
+    meta_file = pixabay_source_dir + 'metadata.pkl'
+    return load_pkl(meta_file)
+
+
+def write_pixabay_metadata_file(metadata):
+    meta_file = pixabay_source_dir + 'metadata.pkl'
+    dump_pkl(meta_file, metadata)
+
+
+def read_pixabay_orphans_file():
+    meta_file = pixabay_source_dir + 'orphans.pkl'
+    return load_pkl(meta_file)
+
+
+def write_pixabay_orphans_file(orphan_metadata):
+    meta_file = pixabay_source_dir + 'orphans.pkl'
+    dump_pkl(meta_file, orphan_metadata)
+
+
+def read_pixabay_tally_file(hit_limit=0):
+    tally_file = pixabay_source_dir + 'tally.txt'
+    with open(tally_file) as ifs:
+        lines = ifs.read().strip().split('\n')
+    tallies = [line.split('\t') for line in lines]
+    tallies = {utils.safe_unicode(label): int(tally) for tally, label in tallies}
+    if hit_limit > 0:
+        tallies = {label: tally for label, tally in tallies.iteritems() if tally > hit_limit}
+    return tallies
+
+
+def write_pixabay_tally_file(label_counts):
+    tally_file = pixabay_source_dir + 'tally.txt'
+    with open(tally_file, 'w') as ofs:
+        for label0, counts in sorted(label_counts.items(), key=itemgetter(1), reverse=True):
+            ofs.write("{}\t{}\n".format(counts, label0))
+
+
+def read_pixabay_aliases_file(dual=False):
+    aliases_file = pixabay_source_dir + 'aliases.txt'
+    with open(aliases_file) as ifs:
+        lines = ifs.read().strip().split('\n')
+    labels_aliases = [line.split('\t') for line in lines]
+    labels, alias_strings = zip(*labels_aliases)
+    alias_lists = [[w.strip() for w in ws.split(',')] for ws in alias_strings]
+    if dual:
+        pixabay_aliases = {alias: label for label, alias_list in zip(labels, alias_lists) for alias in alias_list}
+    else:
+        pixabay_aliases = {label: alias_list for label, alias_list in zip(labels, alias_lists)}
+    return pixabay_aliases
+
+
+def read_pixabay_blacklist_file():
+    blacklist_file = pixabay_source_dir + 'blacklist.txt'
+    with open(blacklist_file) as ifs:
+        pixabay_blacklist = set(ifs.read().strip().lower().split('\n'))
+    return pixabay_blacklist
+
+
+def remove_orphaned_images():
+    metadata = read_pixabay_metadata_file()
+    orphaned_files = []
+    for image_file in os.listdir(pixabay_image_dir):
+        key = int(image_file.split('.')[0])
+        if key not in metadata:
+            orphaned_files.append(image_file)
+
+    for orphaned_file in orphaned_files:
+        source_file = pixabay_image_dir + orphaned_file
+        target_file = pixabay_orphans_dir + orphaned_file
+        shutil.move(source_file, target_file)
+
+    print '{} images orphaned'.format(len(orphaned_files))
+    return
+
+
+def merge_orphaned_images():
+    for orphaned_file in os.listdir(pixabay_orphans_dir):
+        source_file = pixabay_orphans_dir + orphaned_file
+        target_file = pixabay_image_dir + orphaned_file
+        shutil.move(source_file, target_file)
+
+
+def remove_orphaned_metadata():
+    metadata = read_pixabay_metadata_file()
+    orphans = read_pixabay_orphans_file()
+
+    dups = list(set.intersection(set(metadata.keys()), set(orphans.keys())))
+    for dup in dups:
+        del orphans[dup]
+
+    orphaned_keys = []
+    for key, meta in metadata.iteritems():
+        filename = "{}/{}.{}".format(pixabay_image_dir, key, "jpg")
+        if not os.path.exists(filename):
+            orphaned_keys.append(key)
+
+    if len(orphaned_keys) == 0:
+        return
+    for key in orphaned_keys:
+        orphans[key] = metadata.pop(key)
+
+    write_pixabay_orphans_file(orphans)
+    write_pixabay_metadata_file(metadata)
+
+    print '{} records orphaned'.format(len(orphaned_keys))
+    return
+
+
+def merge_orphaned_metadata():
+    metadata = read_pixabay_metadata_file()
+    orphans = read_pixabay_orphans_file()
+
+    if len(orphans) == 0:
+        return
+    for key, orphan in orphans.iteritems():
+        metadata[key] = orphan
+
+    os.remove(pixabay_source_dir + 'orphans.pkl')
+    write_pixabay_metadata_file(metadata)
+
+
+def update_files(metadata):
+    write_pixabay_metadata_file(metadata)
+    remove_orphaned_metadata()
+    metadata = read_pixabay_metadata_file()
+    print 'metadata file saved. {} total records'.format(len(metadata))
+    label_counts = utils.get_counts(metadata)
+    write_pixabay_tally_file(label_counts)
+    print 'tally file saved. {} unique labels.'.format(len(label_counts))
+
+
+def convert_png_to_jpg():
+    for filename in os.listdir(pixabay_image_dir):
+        if filename.endswith('.png'):
+            new_filename = ''.join([filename[:-3], 'jpg'])
+            new_filename = pixabay_image_dir + new_filename
+            if os.path.exists(new_filename):
+                os.remove(new_filename)
+            filename = pixabay_image_dir + filename
+            im = Image.open(filename)
+            im.save(new_filename, "JPEG")
+            os.remove(filename)
+
---
+++ utils.py
@@ -0,0 +1,299 @@
+
+def to_bytestring(s, enc='utf-8'):
+    """Convert the given unicode string to a bytestring, using the standard encoding,
+    unless it's already a bytestring"""
+    return s.encode(enc) if not isinstance(s, str) else s
+
+
+def safe_unicode(obj, *args):
+    """ return the unicode representation of obj """
+    try:
+        return unicode(obj, *args)
+    except UnicodeDecodeError:
+        # obj is byte string
+        ascii_text = str(obj).encode('string_escape')
+        return unicode(ascii_text)
+
+
+def safe_bytestring(obj):
+    """ return the byte string representation of obj """
+    try:
+        return str(obj)
+    except UnicodeEncodeError:
+        # obj is unicode
+        return unicode(obj).encode('unicode_escape')
+
+
+def decode_list(data):
+    rv = []
+    for item in data:
+        if isinstance(item, unicode):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = decode_list(item)
+        elif isinstance(item, dict):
+            item = decode_dict(item)
+        rv.append(item)
+    return rv
+
+
+def decode_dict(data):
+    rv = {}
+    for key, value in data.iteritems():
+        if isinstance(key, unicode):
+            key = key.encode('utf-8')
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        elif isinstance(value, list):
+            value = decode_list(value)
+        elif isinstance(value, dict):
+            value = decode_dict(value)
+        rv[key] = value
+    return rv
+
+
+def iou(a, b):
+    return len(a.intersection(b))/len(a.union(b))
+
+
+class BBoxDim(object):
+
+    def __init__(self):
+        self._min = None
+        self._max = None
+        self._cen = None
+        self._thick = None
+        self.n_inputs = 0
+
+    def reset(self):
+        self.__init__()
+
+    def update(self):
+        if self.n_inputs != 2:
+            return
+        if self._min and self._max:
+            self._cen = (self._max + self._min) / 2
+            self._thick = self._max - self._min
+        elif self._min and self._cen:
+            self._max = 2 * self._cen - self._min
+            self._thick = 2 * (self._cen - self._min)
+        elif self._min and self._thick:
+            self._max = self._min + self._thick
+            self._cen = self._min + self._thick / 2
+        elif self._max and self._cen:
+            self._min = 2 * self._cen - self._max
+            self._thick = 2 * (self._max - self._cen)
+        elif self._max and self._thick:
+            self._min = self._max - self._thick
+            self._cen = self._max - self._thick / 2
+        elif self._cen and self._thick:
+            self._min = self._cen - self._thick / 2
+            self._max = self._cen + self._thick / 2
+        self.n_inputs = 4
+
+    @property
+    def min(self):
+        return self._min
+
+    @min.setter
+    def min(self, val):
+        if self._min is None:
+            self.n_inputs += 1
+            self._min = val
+            self.update()
+
+    @property
+    def max(self):
+        return self._max
+
+    @max.setter
+    def max(self, val):
+        if self._max is None:
+            self.n_inputs += 1
+            self._max = val
+            self.update()
+
+    @property
+    def cen(self):
+        return self._cen
+
+    @cen.setter
+    def cen(self, val):
+        if self._cen is None:
+            self.n_inputs += 1
+            self._cen = val
+            self.update()
+
+    @property
+    def thick(self):
+        return self._thick
+
+    @thick.setter
+    def thick(self, val):
+        if self._thick is None:
+            self.n_inputs += 1
+            self._thick = val
+            self.update()
+
+
+class BoundingBox(object):
+    """
+    size is a 2 element list
+    bbox is a dictionary
+    fmt is the format of the bbox. (i.e. voc, coco, fgvc, darknet
+    """
+    def __init__(self, size, bbox, fmt):
+        self.fmt = fmt
+        self.dw = 1. / size[0]
+        self.dh = 1. / size[1]
+        self.bbox_orig = bbox
+
+        self.bbox_formats = {'voc': ('xmin', 'xmax', 'ymin', 'ymax'),
+                             'fgvc': ('xmin', 'ymin', 'xmax', 'ymax'),
+                             'coco': ('xmin', 'ymin', 'xthick', 'ythick'),
+                             'darknet': ('xcen', 'ycen', 'xthick', 'ythick')}
+
+        self.x = BBoxDim()
+        self.y = BBoxDim()
+
+        for key, val in zip(self.bbox_formats[fmt], bbox):
+            if key[0] in ('x', 'X'):
+                if fmt == 'darknet':
+                    val = int(round(val / self.dw))
+                if key == 'xmin':
+                    self.x.min = val
+                elif key == 'xmax':
+                    self.x.max = val
+                elif key == 'xcen':
+                    self.x.cen = val
+                elif key == 'xthick':
+                    self.x.thick = val
+            if key[0] in ('y', 'Y'):
+                if fmt == 'darknet':
+                    val = int(round(val / self.dh))
+                if key == 'ymin':
+                    self.y.min = val
+                elif key == 'ymax':
+                    self.y.max = val
+                elif key == 'ycen':
+                    self.y.cen = val
+                elif key == 'ythick':
+                    self.y.thick = val
+
+    def convert_to(self, fmt):
+
+        bbox = []
+        for key in self.bbox_formats[fmt]:
+            val = 0
+            if key[0] in ('x', 'X'):
+                if key == 'xmin':
+                    val = self.x.min
+                elif key == 'xmax':
+                    val = self.x.max
+                elif key == 'xcen':
+                    val = self.x.cen
+                elif key == 'xthick':
+                    val = self.x.thick
+                else:
+                    raise
+                if fmt == 'darknet':
+                    val = val * self.dw
+            if key[0] in ('y', 'Y'):
+                if key == 'ymin':
+                    val = self.y.min
+                elif key == 'ymax':
+                    val = self.y.max
+                elif key == 'ycen':
+                    val = self.y.cen
+                elif key == 'ythick':
+                    val = self.y.thick
+                else:
+                    raise
+                if fmt == 'darknet':
+                    val = val * self.dh
+            bbox.append(val)
+        return bbox
+
+
+def convert_coco_bbox(size, box):
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = box[0] + box[2] / 2.0
+    y = box[1] + box[3] / 2.0
+    w = box[2]
+    h = box[3]
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return x, y, w, h
+
+
+def convert_bbox(size, box):
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = (box[0] + box[1]) / 2.0
+    y = (box[2] + box[3]) / 2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return x, y, w, h
+
+
+def print_class_counts(name, class_counts, skip_zeros=False, mapper=None):
+    print '--------{}--------'.format(name)
+    for label, count in class_counts.iteritems():
+        if skip_zeros and count == 0:
+            continue
+        lbl = mapper[label] if mapper else label
+        print '{:15s} {:10d}'.format(lbl, count)
+    print '------------------------'
+
+
+def check_word(word, words_set, suffix=None):
+    if suffix:
+        if word.endswith(suffix):
+            test_word = word.rstrip(suffix)
+    else:
+        test_word = word
+
+    iword = words_set.intersection({test_word})
+    if len(iword) == 0:
+        return 0
+    elif len(iword) == 1:
+        return 1
+    elif len(iword) > 1:
+        return 2
+
+
+def get_counts(md):
+    lc = {}
+    for record in md.itervalues():
+        for tag in record['tags']:
+            if tag not in lc:
+                lc[tag] = 0
+            lc[tag] += 1
+    return lc
+
+
+def simplify_json(meta_old, keys_to_keep):
+    meta_new = {}
+    for name, record_old in meta_old.iteritems():
+        record_new = {}
+        for key, value in record_old.iteritems():
+            if key in keys_to_keep:
+                record_new[key] = value
+        meta_new[name] = record_new
+    return meta_new
+
+
+def extract_tags(meta_old):
+    meta_new = {}
+    for idx, record_old in meta_old.iteritems():
+        meta_new[int(idx)] = record_old['tags']
+    return meta_new
+
+
